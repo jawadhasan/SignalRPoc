cbe0c48c9a5a04073894e3b0c4a57098a76f2efc
Shani <Shani@hds.eu>
2019-01-24 16:59:31 +0100

    consoleClient

---
+++ SignalrPoc/ConsoleClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press a key to start listening!");
+            Console.ReadLine();
+
+            //similar to js
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:41695/coffeeHub")
+                .Build();
+
+            //incoming function
+            connection.On<Order>("NewOrder", (order) =>
+            {
+                Console.WriteLine($"Somebody ordered on {order.Product}");
+            });
+
+            connection.StartAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Listening....Read a key to quit");
+            Console.ReadKey();
+
+
+        }
+    }
+
+    public class Order
+    {
+        public string Product { get; set; }
+        public string Size { get; set; }
+    }
+}
